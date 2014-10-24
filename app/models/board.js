c47e84205897c6fc9379ce9bf3b69e4e470b8c4f
Sherlock Yao <yaosherlock@gmail.com>
2014-10-24 10:52:10 +0800

    add models functions

--- app/models/board.js
+++ app/models/board.js
@@ -9,9 +9,16 @@ var BoardSchema = mongoose.Schema({
 })
 
 BoardSchema.methods = {
-  
 }
 
 BoardSchema.statics = {
-  
+  lookup : function(lat, lon, callback) {
+    query = {
+      lat: { $gt: (lat - 0.5), $lt: (lat + 0.5) },
+      lon: { $gt: (lon - 0.5), $lt: (lon + 0.5) }
+    }
+    this.findOne(query).exec(callback)
+  }  
 }
+
+mongoose.model('Board', BoardSchema)
---
+++ app/models/message.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+
+var MessageSchema = mongoose.Schema({
+  boardId: String,
+  orderNumber: Number,
+  poster: String,
+  content: String
+})
+
+MessageSchema.statics = {
+  messagesOfBoard: function(boardId, callback) {
+    query = { boardId: boardId }
+    this.find(query).exec(callback)
+  }
+}
+
+mongoose.model('Message', MessageSchema)
